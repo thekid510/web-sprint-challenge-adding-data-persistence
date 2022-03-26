7cadcc3d7a63d50414c32ebfe3ee2a87fee970c8
thekid510 <76237067+thekid510@users.noreply.github.com>
2022-03-25 17:27:08 -0700

    server up & dependencies + scripts

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here and require it from index.js
+const express = require('express')
+
+const server = express();
+
+server.use(express.json());
+
+server.use((err, req, res, next) => {// eslint-disable-line
+    res.status(500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  });
+
+module.exports = server
